ee15be7ddcb510df984700b0b3cde8d0417ccbb7
YongLin <linyongw@gmail.com>
2017-12-20 15:40:10 +0800

    修改orderbiz

--- order-service/src/main/java/com/cupdata/order/dao/ServiceOrderDao.java
+++ order-service/src/main/java/com/cupdata/order/dao/ServiceOrderDao.java
@@ -1,6 +1,6 @@
 package com.cupdata.order.dao;
 
-import com.cupdata.order.entity.ServiceOrder;
+import com.cupdata.commons.model.ServiceOrder;
 import org.apache.ibatis.annotations.Mapper;
 import com.cupdata.commons.dao.BaseDao;
 
