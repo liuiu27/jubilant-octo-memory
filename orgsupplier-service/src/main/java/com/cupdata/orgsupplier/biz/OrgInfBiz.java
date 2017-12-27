8773a1ab36f3b6b39bffeeedaeb7d45784bc3a18
liwei <sh_liwei@macrosky.com.cn>
2017-12-27 09:58:52 +0800

    修改

--- orgsupplier-service/src/main/java/com/cupdata/orgsupplier/biz/OrgInfBiz.java
+++ orgsupplier-service/src/main/java/com/cupdata/orgsupplier/biz/OrgInfBiz.java
@@ -11,12 +11,14 @@ import com.cupdata.commons.vo.BaseResponse;
 import com.cupdata.commons.vo.orgsupplier.OrgInfVo;
 import com.cupdata.orgsupplier.dao.OrgInfDao;
 
+import lombok.extern.slf4j.Slf4j;
+
 /**
  * @Auth: LinYong
  * @Description:
  * @Date: 20:20 2017/12/14
  */
-
+@Slf4j
 @Service
 public class OrgInfBiz extends BaseBiz<OrgInf> {
     @Autowired
@@ -31,12 +33,14 @@ public class OrgInfBiz extends BaseBiz<OrgInf> {
 	public BaseResponse<OrgInfVo> findOrgByNo(String orgNo) {
 		BaseResponse<OrgInfVo> BaseResponse =  new BaseResponse<OrgInfVo>();
     	OrgInf orgInf = orgInfDao.findOrgByNo(orgNo);
-    	OrgInfVo orgInfVo = new OrgInfVo();
-    	orgInfVo.setOrgInf(orgInf);
-    	if(null != orgInfVo) {
+    	if(null != orgInf) {
+    		OrgInfVo orgInfVo = new OrgInfVo();
+    		orgInfVo.setOrgInf(orgInf);
     		BaseResponse.setData(orgInfVo);
     		BaseResponse.setResponseCode(ResponseCodeMsg.SUCCESS.getCode());
     		BaseResponse.setResponseMsg(ResponseCodeMsg.SUCCESS.getMsg());
+    	}else {
+    		log.error("findOrgByNo orgNo = " + orgNo + " result is null");
     	}
 		return BaseResponse;
 	}
