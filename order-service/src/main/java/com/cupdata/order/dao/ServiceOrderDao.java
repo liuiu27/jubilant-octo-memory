e6733f36a088b3021fdb37ad88c6d16011a2ba50
Dcein <dcein@163.com>
2018-02-27 20:07:03 +0800

    删除无用的包

--- order-service/src/main/java/com/cupdata/order/dao/ServiceOrderDao.java
+++ order-service/src/main/java/com/cupdata/order/dao/ServiceOrderDao.java
@@ -1,10 +1,8 @@
 package com.cupdata.order.dao;
 
-import org.apache.ibatis.annotations.Mapper;
-import org.apache.ibatis.annotations.Param;
-
 import com.cupdata.commons.dao.BaseDao;
 import com.cupdata.commons.model.ServiceOrder;
+import org.apache.ibatis.annotations.Mapper;
 
 @Mapper
 public interface ServiceOrderDao extends BaseDao<ServiceOrder>{
