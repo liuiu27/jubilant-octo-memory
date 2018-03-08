2c2cca328d818368c75bb4667b24946403479c63
Dcein <dcein@163.com>
2018-03-08 15:46:10 +0800

    爱奇艺服务启动入口

---
+++ iqiyi-service/src/main/java/com.cupdata.iqiyi/IqiyiServiceApplication.java
@@ -0,0 +1,20 @@
+package com.cupdata.iqiyi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+import org.springframework.cloud.netflix.feign.EnableFeignClients;
+
+/**
+ * @Author: DingCong
+ * @Description: 爱奇艺启动入口
+ * @CreateDate: 2018/2/6 14:49
+ */
+@SpringBootApplication
+@EnableEurekaClient
+@EnableFeignClients
+public class IqiyiServiceApplication {
+    public static void main(String[] args){
+        SpringApplication.run(IqiyiServiceApplication.class,args);
+    }
+}
