02450a35491c96afcd4dd26fa45ce8c62ace4fb2
Dcein <dcein@163.com>
2018-03-08 15:34:49 +0800

    互亿话费充值业务

---
+++ commons/src/main/java/com/cupdata/commons/api/ihuyi/IHuyiPhoneController.java
@@ -0,0 +1,15 @@
+package com.cupdata.commons.api.ihuyi;
+
+import com.cupdata.commons.api.recharge.IRechargeAPI;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+/**
+ * @Author: DingCong
+ * @Description: 互亿充值
+ * @CreateDate: 2018/3/6 15:28
+ */
+@RequestMapping("/ihuyiPhoneRecharge")
+public interface IHuyiPhoneController extends IRechargeAPI {
+
+
+}
