ea6f013321408368986ac3a65bb4ed34922cfc15
liwei <sh_liwei@macrosky.com.cn>
2018-03-21 17:02:51 +0800

    修改

--- product-service/src/main/java/com/cupdata/product/ProductServiceApplication.java
+++ product-service/src/main/java/com/cupdata/product/ProductServiceApplication.java
@@ -2,10 +2,12 @@ package com.cupdata.product;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+import org.springframework.cloud.netflix.feign.EnableFeignClients;
 
 @SpringBootApplication
-@EnableDiscoveryClient
+@EnableEurekaClient
+@EnableFeignClients
 public class ProductServiceApplication {
 
 	public static void main(String[] args) {
