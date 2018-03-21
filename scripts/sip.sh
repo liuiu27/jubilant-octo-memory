841dd4ab846aa2df30d21e6c13c711132c87346e
Tony.cup <li926893@163.com>
2018-03-21 16:23:33 +0800

    junliang

--- scripts/sip.sh
+++ scripts/sip.sh
@@ -18,3 +18,46 @@ echo "execute parameter： $1  $2"
 
 ./api-gateway.sh $1 $2
 echo "=== execute gateway"
+
+./cdd-service.sh $1 $2
+echo "=== execute cdd-service"
+
+./config-service.sh $1 $2
+echo "=== execute config-service"
+
+./iqiyi-service.sh $1 $2
+echo "=== execute iqiyi-service"
+
+./lakala-service.sh $1 $2
+echo "=== execute lakala-service"
+
+./notify-service.sh $1 $2
+echo "=== execute notify-service"
+
+./order-service.sh $1 $2
+echo "=== execute order-service"
+
+./orgsupplier-service.sh $1 $2
+echo "=== execute orgsupplier-service"
+
+./product-service.sh $1 $2
+echo "=== execute product-service"
+
+./recharge-service.sh $1 $2
+echo "=== execute recharge-service"
+
+./tencent-service.sh $1 $2
+echo "=== execute tencent-service"
+
+./trvok-service.sh $1 $2
+echo "=== execute trvok-service"
+
+./voucher-service.sh $1 $2
+echo "=== execute voucher-service"
+
+sleep 10
+
+./cache-service.sh $1 $2
+echo "=== execute cache-service"
+
+exit 0
