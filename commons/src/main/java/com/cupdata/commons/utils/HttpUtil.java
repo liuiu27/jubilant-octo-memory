2e5d51f1c224ed8573a1398fc3d7e3d542905d32
liwei <sh_liwei@macrosky.com.cn>
2018-03-28 17:04:19 +0800

    merging

--- commons/src/main/java/com/cupdata/commons/utils/HttpUtil.java
+++ commons/src/main/java/com/cupdata/commons/utils/HttpUtil.java
@@ -357,13 +357,12 @@ public class HttpUtil {
 //		req.setTimestamp(timestamp);
 
 		//获取空港券码请求参数
-<<<<<<< HEAD
-		GetVoucherReq req = new GetVoucherReq();
-		req.setTimestamp(timestamp);
-		req.setExpire("20180327");
-		req.setProductNo("20180108V124");
-		req.setOrgOrderNo("13213213166116");
-		req.setOrderDesc("空港测试");
+//		GetVoucherReq req = new GetVoucherReq();
+//		req.setTimestamp(timestamp);
+//		req.setExpire("20180327");
+//		req.setProductNo("20180108V124");
+//		req.setOrgOrderNo("13213213166116");
+//		req.setOrderDesc("空港测试");
 		
 		//空港禁用券码请求参数
 //		DisableVoucherReq req =  new DisableVoucherReq();
@@ -372,22 +371,7 @@ public class HttpUtil {
 //		req.setTimestamp(timestamp);
 //		req.setVoucherCode("246716108");
 		
-=======
-//		GetVoucherReq req = new GetVoucherReq();
-//		req.setTimestamp(timestamp);
-//		req.setExpire("20180327");
-//		req.setProductNo("20180108V124");
-//		req.setOrgOrderNo("13213213166");
-//		req.setOrderDesc("空港测试");
 
-		//空港禁用券码请求参数
-		DisableVoucherReq req =  new DisableVoucherReq();
-		req.setDisableDesc("禁用测试");
-		req.setOrgOrderNo("13213213166");
-		req.setTimestamp(timestamp);
-		req.setVoucherCode("246716108");
-
->>>>>>> d7fcbd070f39a7e8f6d54c74aeddb18d47a0937e
 		//获取车点点券码请求参数
 		GetVoucherReq getVoucherReq = new GetVoucherReq();
 //		getVoucherReq.setTimestamp(timestamp);
@@ -435,7 +419,6 @@ public class HttpUtil {
 //				"&sign=" + authReqSign ,
 //				"application/x-www-form-urlencoded;charset=UTF-8");
 		
-<<<<<<< HEAD
 		//获取券码URL
 		String data =  doPost("http://localhost:46959/voucher/voucher/getVoucher", "org=20180208O21995540&data=" + reqData + 
 				"&sign=" + authReqSign ,
@@ -446,14 +429,13 @@ public class HttpUtil {
 //				"&sign=" + authReqSign ,
 //				"application/x-www-form-urlencoded;charset=UTF-8");
 			
-=======
 //		String  data = doPost("http://cvpa.leagpoint.com/sipService/voucher/voucher/getVoucher", "org=20180208O21995540&data=" + reqData +
 //				"&sign=" + authReqSign ,
 //				"application/x-www-form-urlencoded;charset=UTF-8");
 		
-		String data = doPost("http://localhost:46959/voucher/voucher/getVoucher", "org=20180208O21995540&data=" + reqData + 
-				"&sign=" + authReqSign ,
-				"application/x-www-form-urlencoded;charset=UTF-8");
+//		String data = doPost("http://localhost:46959/voucher/voucher/getVoucher", "org=20180208O21995540&data=" + reqData + 
+//				"&sign=" + authReqSign ,
+//				"application/x-www-form-urlencoded;charset=UTF-8");
 		
 //		String data = doPost("http://localhost:8040/content/content/contentJump", "org=20180208O21995540&data=" + reqData + 
 //				"&sign=" + authReqSign ,
@@ -461,7 +443,6 @@ public class HttpUtil {
 		
 		//String data = "Gb%2FDZbw8TY2BPpWTFVrVZyHOGYdeClprnES4xBEI4sAMgcA63w%2BRdzJr2RPKgtWHpAcX9QcHEQySrrB2eZiOrNgBtvMMWJ62scv2bbWF3zB%2FiIsWJwqURGNGSCk1tACD5PvcuicOEcMeGSTiyxXSJLmpuQO5DSYwIybRMt2685cLfyvBH0BjXichzjYi8qjYoirs76JIi3AUnVkaFAx86%2FifsoBS7I04OhFshMDbX0bj4z4miDlGeQRUZSou7yO6uqY5h7JM5O023yujfDynRFkIoMS3eoYUcQu6MYJB5PSx86GH1Tkp8TP8SDqYg9jrJDWwhW5U5NTKusdPD22PbQ%3D%3D";
 		
->>>>>>> d7fcbd070f39a7e8f6d54c74aeddb18d47a0937e
         String [] a =	data.split("&sign=");
 		data = a[0].substring(5, a[0].length());
 		data = URLDecoder.decode(data,"utf-8");
