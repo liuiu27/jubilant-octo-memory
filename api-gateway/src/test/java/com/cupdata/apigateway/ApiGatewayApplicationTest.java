82b8b1a8f2c6f9c688c884b523481690b507dcf2
Dcein <dcein520@163.com>
2018-03-29 16:46:36 +0800

    网关单元测试

--- api-gateway/src/test/java/com/cupdata/apigateway/ApiGatewayApplicationTest.java
+++ api-gateway/src/test/java/com/cupdata/apigateway/ApiGatewayApplicationTest.java
@@ -8,18 +8,17 @@ import org.springframework.test.context.junit4.SpringRunner;
 
 
 //下面两个注解用于标记为springboot 的测试，需要初始化spring上下文环境
-@RunWith(SpringRunner.class)
-@SpringBootTest
+/*@RunWith(SpringRunner.class)
+@SpringBootTest*/
 public class ApiGatewayApplicationTest {
 
 
     @Test
-    public  void test(String[] args) {
+    public  void test() {
         Integer a=127,b=127;
         System.out.println(a==b);
         Integer c=124,d=124;
         System.out.println(c==d);
-
         int s=128,f=128;
         System.out.println(s==f);
 
