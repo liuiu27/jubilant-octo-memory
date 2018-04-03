a7efeab6b070d2fd6e56ec2dccf68be38f66b161
Dcein <dcein520@163.com>
2018-04-03 20:30:20 +0800

    新增根据订单编号查询主订单接口

--- commons/src/main/java/com/cupdata/commons/api/ihuyi/IHuyiPhoneController.java
+++ commons/src/main/java/com/cupdata/commons/api/ihuyi/IHuyiPhoneController.java
@@ -2,6 +2,12 @@ package com.cupdata.commons.api.ihuyi;
 
 import com.cupdata.commons.api.recharge.IRechargeAPI;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
 
 /**
  * @Author: DingCong
@@ -10,4 +16,13 @@ import org.springframework.web.bind.annotation.RequestMapping;
  */
 @RequestMapping("/ihuyiPhoneRecharge")
 public interface IHuyiPhoneController extends IRechargeAPI {
+
+    /**
+     * 互亿话费充值异步通知接口
+     * @param request
+     * @param response
+     * @throws IOException
+     */
+    @RequestMapping(value = "ihuyiPhoneRechargeCallBack", method = {RequestMethod.POST})
+    public void ihuyiPhoneRechargeCallBack(HttpServletRequest request, HttpServletResponse response) throws IOException;
 }
--- commons/src/main/java/com/cupdata/commons/api/ihuyi/IhuyiVirtualGoodsController.java
+++ commons/src/main/java/com/cupdata/commons/api/ihuyi/IhuyiVirtualGoodsController.java
@@ -2,6 +2,11 @@ package com.cupdata.commons.api.ihuyi;
 
 import com.cupdata.commons.api.recharge.IRechargeAPI;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
 
 /**
  * @Author: DingCong
@@ -10,4 +15,13 @@ import org.springframework.web.bind.annotation.RequestMapping;
  */
 @RequestMapping("/ihuyiVirtualRecharge")
 public interface IhuyiVirtualGoodsController extends IRechargeAPI {
+
+    /**
+     * 互亿虚拟商品充值通知接口
+     * @param request
+     * @param response
+     * @throws IOException
+     */
+    @RequestMapping(value = "ihuyiVirtualRechargeCallBack",method = {RequestMethod.POST})
+    public void ihuyiVirtualRechargeCallBack(HttpServletRequest request, HttpServletResponse response) throws IOException;
 }
