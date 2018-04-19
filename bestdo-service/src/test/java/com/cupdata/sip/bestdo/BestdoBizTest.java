11af732abd9f41c3717734fe351c340cfea56042
Tony.cup <li926893@163.com>
2018-04-19 10:33:15 +0800

    junliang

--- bestdo-service/src/test/java/com/cupdata/sip/bestdo/BestdoBizTest.java
+++ bestdo-service/src/test/java/com/cupdata/sip/bestdo/BestdoBizTest.java
@@ -2,6 +2,7 @@ package com.cupdata.sip.bestdo;
 
 import com.cupdata.sip.bestdo.biz.BestdoBiz;
 import com.cupdata.sip.bestdo.vo.response.MerInfoRes;
+import com.cupdata.sip.bestdo.vo.response.SaidianOrderRes;
 import com.cupdata.sip.common.dao.entity.ServiceSupplier;
 import com.cupdata.sip.common.dao.mapper.ServiceSupplierMapper;
 import lombok.extern.slf4j.Slf4j;
@@ -52,7 +53,8 @@ public class BestdoBizTest {
     public void testcreateorder() {
 
         String a="{\"bookDay\":\"2018-04-18\",\"bookPhone\":\"13564569994\",\"cupdOrderNo\":\"180416GD055145\",\"items\":[{\"endHour\":\"22\",\"playTime\":\"07:00\",\"startHour\":\"07\"}],\"merItemId\":\"10201021000060\",\"merid\":\"1020102\",\"note\":\"\",\"rightProduct\":\"300\",\"sportType\":\"108\",\"venueNo\":\"1080003412001\"}";
-        bestdoBiz.crateBestdoOrder(a);
+        SaidianOrderRes saidianOrderRes = bestdoBiz.crateBestdoOrder(a);
+
 
     }
 
