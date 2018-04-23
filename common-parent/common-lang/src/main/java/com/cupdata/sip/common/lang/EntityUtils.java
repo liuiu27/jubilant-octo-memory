e689416ff85207373da5fd19515752aa040d8931
liwei <sh_liwei@macrosky.com.cn>
2018-04-23 19:03:59 +0800

    i

--- common-parent/common-lang/src/main/java/com/cupdata/sip/common/lang/EntityUtils.java
+++ common-parent/common-lang/src/main/java/com/cupdata/sip/common/lang/EntityUtils.java
@@ -17,10 +17,10 @@ import java.util.Date;
 public class EntityUtils {
 
     // 默认属性
-    public final static String[] cfields = {"createby","createdate"};
+    public final static String[] cfields = {"createBy","createDate"};
 
     // 默认属性
-    public final static String[] ufields = {"updateby","updatedate"};
+    public final static String[] ufields = {"updateBy","updateDate"};
 
 
     public static <T> void setEntityInfo(T entity,String[] fields){
